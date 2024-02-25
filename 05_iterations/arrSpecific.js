00c95af194eff8ff5c0b1bf161ecbf7e64283286
Swarup Deb <55588687+SwarupDeb@users.noreply.github.com>
2024-02-25 18:24:02 +0000

    day5-end

---
+++ 05_iterations/arrSpecific.js
@@ -0,0 +1,51 @@
+ const arr = [1,2,3,4,5,6,7,8,9,10]
+
+ for (const num of arr) {
+     console.log(num)
+    
+ }
+
+ //Maps
+
+ const map = new Map();
+ map.set('name', 'Swarup');
+
+ const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
---
+++ 05_iterations/while_dowhile.js
@@ -0,0 +1,19 @@
+let i = 0;
+while (i <= 10) {
+  // console.log(i);
+  i = i + 3;
+}
+
+let myArray = [1, 2, 3, 4, 5, 6];
+
+let x = 0;
+while (x < myArray.length) {
+//   console.log(myArray[x]);
+  x++;
+}
+
+let score = 11;   // one time score will get print when the loop runs
+do {
+  console.log(score);
+  score++;
+} while (score <= 10);
